cd4ad86dedbde89cf805fe87e4b4bf97a8919fc6
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2016-12-26 22:33:24 +0200

    Create Users migrations #16

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,33 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('nickname')->nullable();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('gender');
+            $table->date('birthdate');
+            $table->string('profile_picture')->nullable();
+            $table->string('hometown')->nullable();
+            $table->string('marital_status')->nullable();
+            $table->longText('about')->nullable();
             $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('phones_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('phone_number');
+            $table->string('user_id');
+            $table->timestamps();
+        });
+
+        Schema::create('friends_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('friend_id');
+            $table->integer('user_id');
+            $table->boolean('state');
             $table->timestamps();
         });
     }
@@ -30,6 +53,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
+        Schema::drop('friends_users');
+        Schema::drop('phones_users');
         Schema::drop('users');
     }
 }
