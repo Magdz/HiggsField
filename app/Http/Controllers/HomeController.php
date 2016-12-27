a5806bdfaf792734a81bbe9c745f79c5c4aea416
naddat <nada.tamer.nt@gmail.com>
2016-12-27 21:32:08 +0200

    Homepage Fixed #18

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,13 +10,30 @@ class HomeController extends Controller
 {
     public function index(){
     	$user_id = Auth::id();
-        $posts = Post::where('user_id', $user_id)->latest()->get();
+    	$friends = Auth::user()->friends()->get();
+        $posts = Post::where('user_id', $user_id)->orWhere('is_public', '0')->latest()->get();
+        foreach($friends as $friend){
+        	$posts = $posts->merge($friend->posts()->latest()->get());
+        }
+
+  //       select * from posts 
+  //       inner join users on users.id=posts.user_id
+  //       where users.id=$user_id;
+  //       union
+  //       select * from posts
+  //       inner join friends_users on friends_users.user_id=posts.user_id
+  //       where friends_users.state=1
+  //       union
+  //	   select * from posts
+  //       inner join friends_users on friends_users.user_id=posts.user_id
+  //       where friends_users.state=0 AND posts.is_public=1;
+
     	return view('profile', compact('posts'));
     }
 
     public function profile(){
     	$user_id = Auth::id();
-        $posts = Post::where('user_id', $user_id)->get();
+        $posts = Post::where('user_id', $user_id)->latest()->get();
     	return view('profile', compact('posts'));
     }
 }
