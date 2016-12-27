20dd7cbcaddf0a08026edf8b50927d81f03bb3e1
naddat <nada.tamer.nt@gmail.com>
2016-12-27 15:05:21 +0200

    Create Post #18

--- app/User.php
+++ app/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
         return $this->hasMany('App\PhoneNumber', 'user_id');
     }
     
+     public function posts(){
+        return $this->hasMany('App\Post', 'user_id');
+    }
+    
 }
