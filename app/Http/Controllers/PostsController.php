46cc149accd181c2ddd32569358dd283b034f8ab
naddat <nada.tamer.nt@gmail.com>
2016-12-27 15:25:42 +0200

    Init PostsController #18

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+
+
+    }
+
+    public function create()
+    {
+
+
+    }
+
+    public function save(Request $request)
+    {
+
+
+    }
+    
+     
+
+}
