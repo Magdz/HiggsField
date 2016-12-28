b9ba12ffc92d4b456ad531a074c9fd35f494a7ee
Dina Nashaat <dina.nashaat@gmail.com>
2016-12-28 02:49:16 +0200

    Fix image in Sidebar

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -2,10 +2,9 @@
         <div id="pic-name" class="white-box row">
             <div id="user-info">
                 <!--Profile Picture-->
-                <div id = "profile-pic" class="white-box row">
+                <div id = "profile-pic" class="white-box row" >
                     @if(Auth::user()->profile_picture)
-                        <img src="{{ Auth::user()->profile_picture }}" 
-                        style = "width:100px; height:100px; border-radius:100px;">
+                        <img src="{{ Auth::user()->profile_picture }}">
                     @else
                         <img src="{{ URL::asset('/') }}images/default_profile.jpg">
                     @endif
@@ -27,6 +26,16 @@
             <div class="col-md-6 white-box" style="padding-top:18px; height:70px; width: 135px; margin-left:10px;">
                 <h4 style="font-size: 15px; ">{{count(Auth::user()->requests)}} Requests</h4>
             </div> 
-            
+        </div>
+
+        <div id = "profile-info" class="row" style="margin-top:10px;">
+            <div class="white-box "style="text-align:left; padding-left:10px; padding-top:10px;">
+                <h4>Profile Info: </h4>
+                    <h5><span style="font-weight:bold">Email:</span> {{Auth::user()->email}}</h5>
+                    <h5><span style="font-weight:bold">Gender:</span>  {{Auth::user()->gender}}</h5>
+                    <h5><span style="font-weight:bold">Birthday:</span>  {{Auth::user()->birthdate}}</h5>
+                    <h5><span style="font-weight:bold">Hometown:</span>  {{Auth::user()->hometown}}</h5>
+                    <h5><span style="font-weight:bold">Marital Status:</span>  {{Auth::user()->marital_status}}</h5>
+            </div>  
         </div>
 </div>
